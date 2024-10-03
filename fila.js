30b114953c4c783309550808a6bad5f8c10fcbd0
WesRush <gualbertoweslley@gmail.com>
2024-10-03 13:56:21 -0300

    codigo completo

--- fila.js
+++ fila.js
@@ -1,15 +1,35 @@
-let fila = []//deve conter o nome e a posição de espera de cada paciente
-
+let fila = []
 let opcao = ""
 
-do{
-    let paciente = ""
+do {
+  let pacientes = ""
+  for (let i = 0; i < fila.length; i++) {
+    pacientes += (i + 1) + "º - " + fila[i] + "\n"
+  }
 
-for (let index = 0; index < fila.length; index++) {
-    const element = array[index];
-    
-}
+  opcao = prompt(
+    "Pacientes:\n" + pacientes +
+    "\nEscolha uma ação:\n1 - Novo paciente\n2 - Consultar paciente\n3 - Sair"
+  )
 
-}while (opcao != "3") {
-    
-}
+  switch (opcao) {
+    case "1":
+      const novoPaciente = prompt("Qual é o nome do paciente?")
+      fila.push(novoPaciente)
+      break
+    case "2":
+      const pacienteConsultado = fila.shift()
+      if (!pacienteConsultado) {
+        alert("Não há pacientes na fila!")
+      } else {
+        alert(pacienteConsultado + " foi removido da fila.")
+      }
+      break
+    case "3":
+      alert("Encerrando...")
+      break
+    default:
+      alert("Opção inválida!")
+      break
+  }
+} while (opcao !== "3");
