b4f2d2b3c30edaa14a52109197a745702b07c684
WesRush <gualbertoweslley@gmail.com>
2024-10-03 13:23:21 -0300

    primeiro commit

---
+++ fila.js
@@ -0,0 +1,15 @@
+let fila = []//deve conter o nome e a posição de espera de cada paciente
+
+let opcao = ""
+
+do{
+    let paciente = ""
+
+for (let index = 0; index < fila.length; index++) {
+    const element = array[index];
+    
+}
+
+}while (opcao != "3") {
+    
+}
